5aa7ac6d83e4bd520635469775a6836b3b314afe
10hin <10hin@outlook.jp>
2018-12-21 00:21:23 +0900

    subscrioption 追加実装

--- src/AbstructCancellableSubscription.java
+++ src/AbstructCancellableSubscription.java
@@ -3,6 +3,16 @@ public abstruct class AbstructCancellableSubscription implements Subscription {
     protected Future<?> 
     @Override
     public void cancel() {
-        
+        Future<?> handler;
+        while (null != (handler = taskHandler.poll()) {
+            if (!handler.isDone()) {
+                handler.cancel(true);
+            }
+        }
+    }
+    protected abstruct List<Future<?>> publish(final long n);
+    @Override
+    public void request(final long n) {
+        this.taskHandlers.addAll(this.publish(n));
     }
 }
