a1e84627d6e85d497e1ff4a16f1f0ffa658511af
10hin <10hin@outlook.jp>
2018-12-21 19:10:45 +0900

    Publisherの内部Subscriptionクラス実装

--- src/AsynchronousPublisher.java
+++ src/AsynchronousPublisher.java
@@ -6,4 +6,23 @@ public class AsynchronousPublisher<T> implements Publisher<T> {
     public void subscribe(final Subscriber<? super T> subscriber) {
     }
 
+    private static class SimpleCancellableSubscription extends AbstructCancellableSubscription {
+        private final Subscriber<?> subscriber;
+        public SimpleCancellableSubscription(final Subscriber<?> subscriber) {
+            this.subscriber = subscriber;
+        }
+        @Override
+        public List<Future<?>> publish(final long n) {
+            if (n <= 0) {
+                final Future<?> callOnError = pool.invoke(() -> {
+                    this.subscriber.onError(new IllegalArgumentException("requested n: " + n);
+                    return null;
+                });
+                return List.of(callOnError);
+            } else {
+                final List<Future<?>> handlers = new ArrayList();
+            }
+        }
+    }
+
 }
